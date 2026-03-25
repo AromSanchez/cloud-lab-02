ded4a782f37e7cb50c544522f6056a3163842e4a
Arom Patrik Sanchez Gutierrez <arom.sanchez@tecsup.edu.pe>
2026-03-25 11:11:06 -0500

    fix: conexion.php

--- conexion.php
+++ conexion.php
@@ -2,9 +2,9 @@
     function conexion(){
         $host = "host=dpg-d71v6phr0fns738t5c10-a.oregon-postgres.render.com";
         $port = "port=5432";
-        $dbname = "test_xpec";
-        $user = "test_xpec_user";
-        $password = "iQyQwsfWRjaOQt4q6QKhF3RXWIAQOmkc";
+        $dbname = "dbname=test_xpec";
+        $user = "user=test_xpec_user";
+        $password = "password=iQyQwsfWRjaOQt4q6QKhF3RXWIAQOmkc";
 
         $db = pg_connect("$host $port $dbname $user $password");
 
