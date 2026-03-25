7dd4962253f6084975fcd8fadeb7baa8a7e4cac9
Arom Patrik Sanchez Gutierrez <arom.sanchez@tecsup.edu.pe>
2026-03-25 11:09:21 -0500

    fix: ruta host

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
     function conexion(){
-        $host = "host=dpg-d71v6phr0fns738t5c10-a.oregon-postgres.render.com/test_xpec";
+        $host = "host=dpg-d71v6phr0fns738t5c10-a.oregon-postgres.render.com";
         $port = "port=5432";
         $dbname = "test_xpec";
         $user = "test_xpec_user";
